2c511c1d62ce209b42261fb8c68feb37c7fbc2a9
Tai Lee <real.human@mrmachine.net>
2016-07-10 23:43:05 +1000

    Refactor what submodules and data files get collected in Django hooks.
    * Get hidden imports for *all* known Django settings that might contain
      a Python dotted path (according to the 1.9 docs), including some that
      were deprecated in 1.8.
    
    * Don't blindly strip class and function names from dotted paths found
      in settings. Instead, catch `ImportError` and progressively strip the
      last fragment until we find a module or package.
    
    * Discover app packages via AppConfig classes in the `INSTALLED_APPS`
      setting.
    
    * Collect submodules *and* data files for hidden imports found in
      the project. In most cases, templates and other data files for
      installed apps (for example) will be required.
    
    * Collect submodules *and* data files for `django` package. It seems
      inconsistent guesswork to collect all data files and not also collect
      all submodules. Likewise, collecting hidden imports required by the
      example project is not likely to be correct for actual projects.
    
    * Collect submodules *and* data files for the project's root package.
    
    * Collect *all* migrations packages discovered in hidden imports (all
      installed apps, all Django apps, and all project apps) as data files,
      instead of only a few hard coded Django app migration packages.
    
    * Don't blindly assume that installed apps will all have `templatetags`
      and `context_processors` modules or packages and explicitly try to
      collect them. Instead, rely on the fact that all submodules and data
      for installed apps, Django, and the project, will be collected.
    
    * Don't assume SQLite database should be collected as a data file.

--- PyInstaller/hooks/hook-django.py
+++ PyInstaller/hooks/hook-django.py
@@ -7,106 +7,73 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-
-# Tested with django 1.8.
-
-
-import sys
-import glob
 import os
-from PyInstaller import log as logging
-from PyInstaller.utils.hooks import django_find_root_dir, django_dottedstring_imports, \
-        collect_data_files, collect_submodules, get_module_file_attribute
+import sys
 
+from PyInstaller import log as logging
+from PyInstaller.utils.hooks import \
+    django_find_root_dir, django_dottedstring_imports, collect_data_files, \
+    collect_submodules, get_module_file_attribute, is_package
 
 logger = logging.getLogger(__name__)
 
 hiddenimports = []
-
+datas = []
 
 root_dir = django_find_root_dir()
 if root_dir:
     logger.info('Django root directory %s', root_dir)
-    # Include imports from the mysite.settings.py module.
-    settings_py_imports = django_dottedstring_imports(root_dir)
-    # Include all submodules of all imports detected in mysite.settings.py.
-    for submod in settings_py_imports:
-        hiddenimports.append(submod)
-        hiddenimports += collect_submodules(submod)
-    # Include main django modules - settings.py, urls.py, wsgi.py.
-    # Without them the django server won't run.
-    package_name = os.path.basename(root_dir)
-    hiddenimports += [
-            # TODO Consider including 'mysite.settings.py' in source code as a data files.
-            #      Since users might need to edit this file.
-            package_name + '.settings',
-            package_name + '.urls',
-            package_name + '.wsgi',
-    ]
-    # Include some hidden modules that are not imported directly in django.
-    hiddenimports += [
-            'django.template.defaultfilters',
-            'django.template.defaulttags',
-            'django.template.loader_tags',
-            'django.template.context_processors',
-    ]
-    hiddenimports += collect_submodules('django.middleware')
-    hiddenimports += collect_submodules('django.templatetags')
-    # Other hidden imports to get Django example startproject working.
-    hiddenimports += [
-            'django.contrib.messages.storage.fallback',
-    ]
-    # Django hiddenimports from the standard Python library.
+
+    # Include hidden imports from the project settings, and their data files.
+    for mod in django_dottedstring_imports(root_dir):
+        if '.'.join(mod.split('.')[:-1]) in hiddenimports:
+            continue  # Already seen
+        if is_package(mod):
+            logger.info('Collecting submodules and data for package: %s' % mod)
+            hiddenimports.extend(collect_submodules(mod))
+            datas.extend(collect_data_files(mod))
+        else:
+            hiddenimports.append(mod)
+
+    # Include all submodules and data files from your project.
+    package = os.path.basename(root_dir)
+    logger.info('Collecting submodules and data for package: %s' % package)
+    hiddenimports += collect_submodules(package)
+    datas += collect_data_files(package)
+
+    # Include all submodules and data files from Django. Some hidden imports
+    # and data files might not be referenced and therefore discovered (e.g.
+    # locales, app and project templates, etc.)
+    logger.info('Collecting submodules and data for package: django')
+    hiddenimports.extend(collect_submodules('django'))
+    datas.extend(collect_data_files('django'))
+
+    # Include known Django hidden imports from the standard Python library.
     if sys.version_info.major == 3:
         # Python 3.x
-        hiddenimports += [
-                'http.cookies',
-                'html.parser',
-        ]
+        hiddenimports.extend([
+            'http.cookies',
+            'html.parser',
+        ])
     else:
         # Python 2.x
-        hiddenimports += [
-                'Cookie',
-                'HTMLParser',
-        ]
+        hiddenimports.extend([
+            'Cookie',
+            'HTMLParser',
+        ])
 
-    # Include django data files - localizations, etc.
-    datas = collect_data_files('django')
-
-    # Bundle django DB schema migration scripts as data files.
-    # They are necessary for some commands.
-    logger.info('Collecting Django migration scripts.')
-    django_mod_dir = os.path.dirname(get_module_file_attribute('django'))
-    migration_modules = [
-             'conf.app_template.migrations',
-             'contrib.admin.migrations',
-             'contrib.auth.migrations',
-             'contrib.contenttypes.migrations',
-             'contrib.flatpages.migrations',
-             'contrib.redirects.migrations',
-             'contrib.sessions.migrations',
-             'contrib.sites.migrations',
-    ]
-    for mod in migration_modules:
-        bundle_dir = mod.replace('.', os.sep)
-        pattern = os.path.join(django_mod_dir, bundle_dir, '*.py')
-        files = glob.glob(pattern)
-        for f in files:
-            datas.append((f, os.path.join('django', bundle_dir)))
-
-    # Include data files from your Django project found in your django root package.
-    datas += collect_data_files(package_name)
-
-    # Include database file if using sqlite. The sqlite database is usually next to the manage.py script.
-    root_dir_parent = os.path.dirname(root_dir)
-    # TODO Add more patterns if necessary.
-    _patterns = ['*.db', 'db.*']
-    for p in _patterns:
-        files = glob.glob(os.path.join(root_dir_parent, p))
-        for f in files:
-            # Place those files next to the executable.
-            datas.append((f, '.'))
-
+    # Include all discovered Django migrations as `.py` data files. It is not
+    # enough to have only the collected `.pyc` modules.
+    for package in hiddenimports:
+        if package.endswith('.migrations'):
+            logger.info(
+                'Collecting migrations as data files for package: %s' %
+                package)
+            bits = package.split('.')
+            datas += collect_data_files(
+                bits[0],
+                include_py_files=True,
+                subdir=os.path.sep.join(bits[1:]))
 
 else:
-    logger.warn('No django root directory could be found!')
+    logger.warn('No Django root directory could be found!')
